7c0e0b56e77c8de11e0e51a635b1bbb0d02e6449
jymun-97 <mjy0750@gmail.com>
2022-06-13 21:50:48 +0900

    feat: Memo Fragment 레이아웃 작성

--- app/src/main/java/com/harusekki/jymun/ui/fragment/MemoFragment.kt
+++ app/src/main/java/com/harusekki/jymun/ui/fragment/MemoFragment.kt
@@ -23,6 +23,6 @@ class MemoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.memoSeekBar.isEnabled = false
     }
 }
