15763c82bcbeb40228a041ab2e780d3c8302e6f8
jymun-97 <mjy0750@gmail.com>
2022-11-09 18:56:19 +0900

    add: Refrigerator Fragment and ViewModel

---
+++ app/src/main/java/com/jymun/harusekki/ui/refrigerator/RefrigeratorFragment.kt
@@ -0,0 +1,19 @@
+package com.jymun.harusekki.ui.refrigerator
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.jymun.harusekki.databinding.FragmentRefrigeratorBinding
+import com.jymun.harusekki.ui.base.BaseFragment
+
+class RefrigeratorFragment : BaseFragment<RefrigeratorViewModel, FragmentRefrigeratorBinding>() {
+
+    override val viewModel: RefrigeratorViewModel by viewModels()
+
+    override fun getViewDataBinding() = FragmentRefrigeratorBinding.inflate(layoutInflater)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tempTextView.text = "나의 냉장고"
+    }
+}
