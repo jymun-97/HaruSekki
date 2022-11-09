93313df1a6b721bf1c930bdffc2a8d6cb3c0b97b
jymun-97 <mjy0750@gmail.com>
2022-11-09 16:07:23 +0900

    modify base recyclerview adapter

--- app/src/main/java/com/jymun/harusekki/ui/base/adapter/ModelRecyclerAdapter.kt
+++ app/src/main/java/com/jymun/harusekki/ui/base/adapter/ModelRecyclerAdapter.kt
@@ -10,24 +10,23 @@ import com.jymun.harusekki.util.resources.ResourcesProvider
 import javax.inject.Inject
 
 class ModelRecyclerAdapter<M : Model> @Inject constructor(
-    private var modelList: List<Model>,
-    private val resourcesProvider: ResourcesProvider,
-    private val adapterListener: AdapterListener
+    private val resourcesProvider: ResourcesProvider
 ) : ListAdapter<Model, ModelViewHolder<M>>(Model.diffUtil) {
 
-    override fun getItemCount(): Int = modelList.size
+    private var adapterListener: AdapterListener? = null
 
-    override fun getItemViewType(position: Int): Int = modelList[position].type.ordinal
+    fun addAdapterListener(listener: AdapterListener) {
+        adapterListener = listener
+    }
+
+    override fun getItemCount(): Int = currentList.size
+
+    override fun getItemViewType(position: Int): Int = currentList[position].type.ordinal
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder<M> =
         ModelViewHolderMapper.map(parent, ModelType.values()[viewType], resourcesProvider)
 
     @Suppress("UNCHECKED_CAST")
     override fun onBindViewHolder(holder: ModelViewHolder<M>, position: Int) =
-        holder.bindData(modelList[position] as M, adapterListener)
-
-    override fun submitList(list: MutableList<Model>?) {
-        list?.let { modelList = it }
-        super.submitList(list)
-    }
+        holder.bindData(currentList[position] as M, adapterListener)
 }
--- app/src/main/java/com/jymun/harusekki/ui/base/adapter/viewholder/ModelViewHolder.kt
+++ app/src/main/java/com/jymun/harusekki/ui/base/adapter/viewholder/ModelViewHolder.kt
@@ -5,12 +5,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.jymun.harusekki.data.model.Model
 import com.jymun.harusekki.ui.base.adapter.AdapterListener
 import com.jymun.harusekki.util.resources.ResourcesProvider
-import javax.inject.Inject
 
-abstract class ModelViewHolder<M : Model> @Inject constructor(
+abstract class ModelViewHolder<M : Model>(
     binding: ViewDataBinding,
     protected val resourcesProvider: ResourcesProvider
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    abstract fun bindData(model: M, adapterListener: AdapterListener)
+    abstract fun bindData(model: M, adapterListener: AdapterListener?)
 }
