a683ec48e7af2d428e3a549021450110adf55137
jymun-97 <mjy0750@gmail.com>
2022-11-09 18:56:02 +0900

    add: Menu Fragment and ViewModel

---
+++ app/src/main/java/com/jymun/harusekki/ui/menu/MenuViewModel.kt
@@ -0,0 +1,11 @@
+package com.jymun.harusekki.ui.menu
+
+import com.jymun.harusekki.ui.base.BaseViewModel
+import com.jymun.harusekki.util.dispatcher.DispatcherProvider
+import javax.inject.Inject
+
+class MenuViewModel @Inject constructor(
+    dispatcherProvider: DispatcherProvider
+) : BaseViewModel(dispatcherProvider) {
+
+}
