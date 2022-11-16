823e01c7e9b60fed06e33d0df1a5e257e7e7031d
jymun-97 <mjy0750@gmail.com>
2022-11-16 17:17:45 +0900

    feat: get dimens resources function in ResourcesProvider

--- app/src/main/java/com/jymun/harusekki/util/resources/ResourcesProvider.kt
+++ app/src/main/java/com/jymun/harusekki/util/resources/ResourcesProvider.kt
@@ -1,6 +1,7 @@
 package com.jymun.harusekki.util.resources
 
 import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 
 interface ResourcesProvider {
@@ -8,4 +9,8 @@ interface ResourcesProvider {
     fun getString(@StringRes resId: Int): String
 
     fun getColor(@ColorRes resId: Int): Int
+
+    fun getDimension(@DimenRes resId: Int): Int
+
+    fun getScreenWidth(): Int
 }
