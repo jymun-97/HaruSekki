3a4186afb5f33c9e05effcae701e5713951c0610
jymun-97 <mjy0750@gmail.com>
2022-11-17 14:04:01 +0900

    feat: show RecipeCategory RecyclerView

--- app/src/main/java/com/jymun/harusekki/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jymun/harusekki/ui/home/HomeFragment.kt
@@ -3,10 +3,15 @@ package com.jymun.harusekki.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
 import com.jymun.harusekki.R
+import com.jymun.harusekki.data.model.recipe.RecipeCategory
 import com.jymun.harusekki.databinding.FragmentHomeBinding
 import com.jymun.harusekki.ui.base.BaseFragment
+import com.jymun.harusekki.ui.base.adapter.ModelRecyclerAdapter
 import com.jymun.harusekki.ui.extensions.showOtherPages
+import com.jymun.harusekki.ui.home.recipe.category.RecipeCategoryGenerator
 import com.jymun.harusekki.ui.home.shortcut.MemoShortcutFragment
 import com.jymun.harusekki.ui.home.shortcut.MenuShortcutFragment
 import com.jymun.harusekki.ui.home.shortcut.RefrigeratorShortcutFragment
@@ -34,6 +39,7 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         initShortcuts()
+        initRecipeCategoryRecyclerView()
     }
 
     private fun initShortcuts() = with(resourcesProvider) {
@@ -48,6 +54,15 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
                 pageWidth = getDimension(R.dimen.shortcut_page_width),
                 screenWidth = getScreenWidth()
             )
+        }
+    }
+
+    private fun initRecipeCategoryRecyclerView() {
+        binding.fragmentHomeContent.recipeCategoryRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireActivity(), HORIZONTAL, false)
+            adapter = ModelRecyclerAdapter<RecipeCategory>(resourcesProvider).apply {
+                submitList(RecipeCategoryGenerator.get())
+            }
         }
     }
 }
