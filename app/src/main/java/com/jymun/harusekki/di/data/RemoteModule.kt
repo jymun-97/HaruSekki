aad887838f9f13a7ac39bc351379f649b89e8d3a
jymun-97 <mjy0750@gmail.com>
2022-11-18 14:30:04 +0900

    feat: create SearchRecipeService Retrofit Client

---
+++ app/src/main/java/com/jymun/harusekki/di/data/RemoteModule.kt
@@ -0,0 +1,43 @@
+package com.jymun.harusekki.di.data
+
+import com.jymun.harusekki.data.service.Constant
+import com.jymun.harusekki.data.service.SearchRecipeService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constant.BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideSearchRecipeService(retrofit: Retrofit): SearchRecipeService =
+        retrofit.create(SearchRecipeService::class.java)
+}
