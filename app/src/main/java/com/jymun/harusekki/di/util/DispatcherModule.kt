afb2f1022d147dacdbd851c85f3968325a89c7f2
jymun-97 <mjy0750@gmail.com>
2022-11-23 18:13:19 +0900

    fix: DispatcherModule install in SingletonComponent

--- app/src/main/java/com/jymun/harusekki/di/util/DispatcherModule.kt
+++ app/src/main/java/com/jymun/harusekki/di/util/DispatcherModule.kt
@@ -5,12 +5,14 @@ import com.jymun.harusekki.util.dispatcher.DispatcherProviderImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class DispatcherModule {
 
     @Binds
+    @Singleton
     abstract fun bindDispatcherProvider(provider: DispatcherProviderImpl): DispatcherProvider
 }
