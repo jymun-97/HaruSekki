b31d589fd323d2ceb582e83f707e5b60dc041c31
jymun-97 <mjy0750@gmail.com>
2022-11-23 18:08:11 +0900

    add: create RemoteDataSourceModule

---
+++ app/src/main/java/com/jymun/harusekki/di/data/RemoteDataSourceModule.kt
@@ -0,0 +1,20 @@
+package com.jymun.harusekki.di.data
+
+import com.jymun.harusekki.data.source.recipe.RecipeDataSource
+import com.jymun.harusekki.data.source.recipe.RecipeRemoteDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindsRecipeRemoteDataSource(
+        recipeRemoteDataSource: RecipeRemoteDataSource
+    ): RecipeDataSource
+}
