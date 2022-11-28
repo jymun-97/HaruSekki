dd00ba3b57a65ba65c2ab4922674ab8df0b85c20
jymun-97 <mjy0750@gmail.com>
2022-11-28 16:37:30 +0900

    test: create TestDispatcherProvider for test

---
+++ app/src/test/java/com/jymun/harusekki/util/TestDispatcherProvider.kt
@@ -0,0 +1,22 @@
+package com.jymun.harusekki.util
+
+import com.jymun.harusekki.util.dispatcher.DispatcherProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+
+@ExperimentalCoroutinesApi
+class TestDispatcherProvider : DispatcherProvider {
+
+    val testDispatcher = StandardTestDispatcher()
+
+    override val main: CoroutineDispatcher
+        get() = testDispatcher
+
+    override val io: CoroutineDispatcher
+        get() = testDispatcher
+
+    override val default: CoroutineDispatcher
+        get() = testDispatcher
+
+}
