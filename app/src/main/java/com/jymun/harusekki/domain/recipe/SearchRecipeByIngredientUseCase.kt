54a12be57a385cee5c7694350a72e0d04c19b84c
jymun-97 <mjy0750@gmail.com>
2022-11-29 17:33:24 +0900

    feat: 레시피 검색 단위 유스케이스 작성 (Detail 제외)

---
+++ app/src/main/java/com/jymun/harusekki/domain/recipe/SearchRecipeByIngredientUseCase.kt
@@ -0,0 +1,44 @@
+package com.jymun.harusekki.domain.recipe
+
+import com.jymun.harusekki.data.model.ModelType
+import com.jymun.harusekki.data.model.recipe.Recipe
+import com.jymun.harusekki.data.repository.RecipeRepository
+import com.jymun.harusekki.ui.home.recipe.RecipeSortBy
+import com.jymun.harusekki.util.dispatcher.DispatcherProvider
+import com.jymun.harusekki.util.exception.CustomExceptions
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SearchRecipeByIngredientUseCase @Inject constructor(
+    private val dispatcherProvider: DispatcherProvider,
+    private val recipeRepository: RecipeRepository
+) {
+
+    suspend operator fun invoke(
+        ingredientList: List<Long>,
+        orderBy: RecipeSortBy = RecipeSortBy.LATEST,
+        refreshFlag: Boolean = false
+    ): List<Recipe> = withContext(dispatcherProvider.default) {
+
+        if (ingredientList.isEmpty()) throw CustomExceptions.NothingSelectedException()
+
+        // TODO. api -> orderBy 파라미터 추가
+        val recipeEntityList = recipeRepository.searchByIngredient(ingredientList)
+        if (recipeEntityList.isEmpty()) throw CustomExceptions.NotDataExistException()
+
+        return@withContext recipeEntityList.map {
+            Recipe(
+                id = it.id,
+                type = ModelType.RECIPE,
+                title = it.title,
+                category = it.category,
+                summary = it.summary,
+                hits = it.hits,
+                likes = it.likes,
+                imgList = it.imgList
+            )
+        }
+    }
+}
---
+++ app/src/test/java/com/jymun/harusekki/domain/RecipeUseCasesTest.kt
@@ -0,0 +1,101 @@
+package com.jymun.harusekki.domain
+
+import com.google.common.truth.Truth.assertThat
+import com.jymun.harusekki.data.cache.recipe.RecipeDataCache
+import com.jymun.harusekki.data.cache.recipe.RecipeDataCacheImpl
+import com.jymun.harusekki.data.entity.recipe.RecipeEntity
+import com.jymun.harusekki.data.recipe.FakeRecipeRemoteDataSource
+import com.jymun.harusekki.data.repository.RecipeRepository
+import com.jymun.harusekki.data.repository.RecipeRepositoryImpl
+import com.jymun.harusekki.data.source.recipe.RecipeDataSource
+import com.jymun.harusekki.domain.recipe.LoadAllRecipeUseCase
+import com.jymun.harusekki.domain.recipe.SearchRecipeByCategoryUseCase
+import com.jymun.harusekki.domain.recipe.SearchRecipeByIngredientUseCase
+import com.jymun.harusekki.domain.recipe.SearchRecipeByTitleUseCase
+import com.jymun.harusekki.util.TestDispatcherProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class RecipeUseCasesTest {
+
+    private val fakeRecipeList = (0 until 100).map {
+        RecipeEntity(
+            id = it.toLong(),
+            title = "Title $it",
+            category = "밥/죽/떡",
+            summary = "Summary $it",
+            imgList = emptyList(),
+            hits = 0,
+            likes = 0,
+            ingredientList = null,
+            cookingStepList = null
+        )
+    }
+    private val testDispatcherProvider = TestDispatcherProvider()
+
+    private lateinit var recipeDataSource: RecipeDataSource
+    private lateinit var recipeDataCache: RecipeDataCache
+    private lateinit var recipeRepository: RecipeRepository
+
+    private lateinit var loadAllRecipeUseCase: LoadAllRecipeUseCase
+    private lateinit var searchRecipeByCategoryUseCase: SearchRecipeByCategoryUseCase
+    private lateinit var searchRecipeByTitleUseCase: SearchRecipeByTitleUseCase
+    private lateinit var searchRecipeByIngredientUseCase: SearchRecipeByIngredientUseCase
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcherProvider.testDispatcher)
+
+        recipeDataSource = FakeRecipeRemoteDataSource(fakeRecipeList)
+        recipeDataCache = RecipeDataCacheImpl(testDispatcherProvider)
+        recipeRepository =
+            RecipeRepositoryImpl(testDispatcherProvider, recipeDataSource, recipeDataCache)
+
+        loadAllRecipeUseCase = LoadAllRecipeUseCase(testDispatcherProvider, recipeRepository)
+        searchRecipeByTitleUseCase =
+            SearchRecipeByTitleUseCase(testDispatcherProvider, recipeRepository)
+        searchRecipeByCategoryUseCase =
+            SearchRecipeByCategoryUseCase(testDispatcherProvider, recipeRepository)
+        searchRecipeByIngredientUseCase =
+            SearchRecipeByIngredientUseCase(testDispatcherProvider, recipeRepository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun loadAll() = runTest {
+        assertThat(
+            loadAllRecipeUseCase().map { it.toEntity() }
+        ).isEqualTo(fakeRecipeList)
+    }
+
+    @Test
+    fun searchByTitle() = runTest {
+        val keyword = "3"
+        assertThat(
+            searchRecipeByTitleUseCase(keyword).map { it.toEntity() }
+        ).isEqualTo(
+            fakeRecipeList.filter { it.title.contains(keyword) }
+        )
+    }
+
+    @Test
+    fun searchByCategory() = runTest {
+        val category = "밥/죽/떡"
+        assertThat(
+            searchRecipeByCategoryUseCase(category).map { it.toEntity() }
+        ).isEqualTo(
+            fakeRecipeList.filter { it.category == category }
+        )
+    }
+}
