322b2806009ba56301b18a6fcdeaab9557749089
jymun-97 <mjy0750@gmail.com>
2022-12-05 20:58:56 +0900

    add: DetailFragment, ViewModel, Layout

---
+++ app/src/main/java/com/jymun/harusekki/ui/detail/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.jymun.harusekki.ui.detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.jymun.harusekki.databinding.FragmentDetailBinding
+import com.jymun.harusekki.ui.base.BaseFragment
+
+class DetailFragment : BaseFragment<DetailViewModel, FragmentDetailBinding>() {
+
+    private val args by navArgs<DetailFragmentArgs>()
+
+    override val viewModel: DetailViewModel by viewModels()
+
+    override fun getViewDataBinding() = FragmentDetailBinding.inflate(layoutInflater)
+
+    override fun setUpBinding() = binding.apply {
+        // TODO("Not yet implemented")
+    }
+
+    override fun observeState() {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textView.text = args.id.toString()
+    }
+}
