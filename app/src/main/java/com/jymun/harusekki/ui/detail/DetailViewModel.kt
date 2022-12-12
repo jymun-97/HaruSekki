8e47f7b3a6ba70916c436a08e03d882e66540fcb
jymun-97 <mjy0750@gmail.com>
2022-12-12 20:58:49 +0900

    feat: DetailViewModel에서 레시피 세부 항목 로드 요청

--- app/src/main/java/com/jymun/harusekki/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/jymun/harusekki/ui/detail/DetailViewModel.kt
@@ -1,5 +1,9 @@
 package com.jymun.harusekki.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.jymun.harusekki.data.model.recipe.RecipeDetail
+import com.jymun.harusekki.domain.recipe.LoadRecipeDetailUseCase
 import com.jymun.harusekki.ui.base.BaseViewModel
 import com.jymun.harusekki.util.dispatcher.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,8 +11,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    dispatcherProvider: DispatcherProvider
+    dispatcherProvider: DispatcherProvider,
+    private val loadRecipeDetailUseCase: LoadRecipeDetailUseCase
 ) : BaseViewModel(dispatcherProvider) {
 
+    private val _recipeDetail = MutableLiveData<RecipeDetail>()
+    val recipeDetail: LiveData<RecipeDetail>
+        get() = _recipeDetail
 
+    fun loadData(recipeId: Long) = onMainDispatcher {
+        _recipeDetail.postValue(loadRecipeDetailUseCase.invoke(recipeId))
+    }
 }
