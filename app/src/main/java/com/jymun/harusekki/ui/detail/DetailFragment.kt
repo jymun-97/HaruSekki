0de0885b71d2d56fd80baa979a4b90a5e314ce9a
jymun-97 <mjy0750@gmail.com>
2022-12-15 00:15:18 +0900

    feat: Recipe Detail Fragment에서 필요 재료 리스트 UI 출력

--- app/src/main/java/com/jymun/harusekki/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/jymun/harusekki/ui/detail/DetailFragment.kt
@@ -5,13 +5,22 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import com.jymun.harusekki.R
+import com.jymun.harusekki.data.model.ingredient.Ingredient
 import com.jymun.harusekki.databinding.FragmentDetailBinding
 import com.jymun.harusekki.ui.base.BaseFragment
 import com.jymun.harusekki.ui.base.LoadState
+import com.jymun.harusekki.ui.base.adapter.ModelRecyclerAdapter
+import com.jymun.harusekki.util.resources.ResourcesProvider
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<DetailViewModel, FragmentDetailBinding>() {
+
+    @Inject
+    lateinit var resourcesProvider: ResourcesProvider
 
     private val args by navArgs<DetailFragmentArgs>()
 
@@ -34,7 +43,22 @@ class DetailFragment : BaseFragment<DetailViewModel, FragmentDetailBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.d("# DetailFragment", "onViewCreated() called")
+        initNeedIngredientRecyclerView()
+
         viewModel.loadData(args.id)
+    }
+
+    private fun initNeedIngredientRecyclerView() {
+        binding.fragmentDetailContent.needIngredientRecyclerView.apply {
+            adapter = ModelRecyclerAdapter<Ingredient>(resourcesProvider)
+            layoutManager = object : GridLayoutManager(
+                requireActivity(),
+                resourcesProvider.getInteger(R.integer.count_showing_ingredient_items),
+                VERTICAL,
+                false
+            ) {
+                override fun canScrollVertically(): Boolean = false
+            }
+        }
     }
 }
