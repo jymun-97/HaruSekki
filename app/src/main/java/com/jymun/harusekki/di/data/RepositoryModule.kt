45f4e3e28032865c3844fb7a9b845caa075d4a4a
jymun-97 <mjy0750@gmail.com>
2023-01-03 23:52:42 +0900

    add: SearchKeyword Repository in RepositoryModule

--- app/src/main/java/com/jymun/harusekki/di/data/RepositoryModule.kt
+++ app/src/main/java/com/jymun/harusekki/di/data/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.jymun.harusekki.di.data
 
 import com.jymun.harusekki.data.repository.RecipeRepository
 import com.jymun.harusekki.data.repository.RecipeRepositoryImpl
+import com.jymun.harusekki.data.repository.SearchKeywordRepository
+import com.jymun.harusekki.data.repository.SearchKeywordRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RepositoryModule {
     abstract fun bindsRecipeRepository(
         recipeRepositoryImpl: RecipeRepositoryImpl
     ): RecipeRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsSearchKeywordRepository(
+        searchKeywordRepositoryImpl: SearchKeywordRepositoryImpl
+    ): SearchKeywordRepository
 }
