fd3c23543fbb6d33f7d2585aba5722bd54179149
jymun-97 <mjy0750@gmail.com>
2023-01-03 23:50:52 +0900

    add: SearchKeyword Local Database

--- app/src/main/java/com/jymun/harusekki/data/entity/recipe/SearchKeywordEntity.kt
+++ app/src/main/java/com/jymun/harusekki/data/entity/recipe/SearchKeywordEntity.kt
@@ -1,8 +1,11 @@
 package com.jymun.harusekki.data.entity.recipe
 
+import androidx.room.PrimaryKey
 import com.jymun.harusekki.data.entity.Entity
 
+@androidx.room.Entity(tableName = "search_keyword")
 data class SearchKeywordEntity(
-    override val id: Long,
+    override val id: Long = 0L,
+    @PrimaryKey(autoGenerate = false)
     val keyword: String
 ) : Entity
--- app/src/main/java/com/jymun/harusekki/data/model/recipe/SearchKeyword.kt
+++ app/src/main/java/com/jymun/harusekki/data/model/recipe/SearchKeyword.kt
@@ -5,7 +5,7 @@ import com.jymun.harusekki.data.model.Model
 import com.jymun.harusekki.data.model.ModelType
 
 data class SearchKeyword(
-    override val id: Long,
+    override val id: Long = 0L,
     override val type: ModelType = ModelType.SEARCH_KEYWORD,
     val keyword: String
 ) : Model(id, type) {
