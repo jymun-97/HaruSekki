14eb3db3d55f8d1e7d41a5db76e68ae4de061a88
jymun-97 <mjy0750@gmail.com>
2023-01-31 21:34:12 +0900

    fix: RecipeCategory Mapping 이 모두 ALL 로 처리되는 버그

--- app/src/main/java/com/jymun/harusekki/ui/home/recipe/category/RecipeCategoryMapper.kt
+++ app/src/main/java/com/jymun/harusekki/ui/home/recipe/category/RecipeCategoryMapper.kt
@@ -4,5 +4,7 @@ object RecipeCategoryMapper {
 
     private val categoryList = RecipeCategory.values()
 
-    fun map(value: String): RecipeCategory = categoryList.find { value.contains(it.value) }!!
+    fun map(value: String): RecipeCategory =
+        categoryList.find { it.value.isNotEmpty() && value.contains(it.value) }
+            ?: RecipeCategory.ALL
 }
