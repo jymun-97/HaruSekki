df8d46c1065ec4099dc4762323450b4b126f9dbd
jymun-97 <mjy0750@gmail.com>
2023-02-18 17:17:30 +0900

    chore: implement MaterialCalendarView

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -43,4 +43,8 @@ object Dependencies {
     const val ROOM = "androidx.room:room-runtime:${Versions.ROOM}"
     const val ROOM_KTX = "androidx.room:room-ktx:${Versions.ROOM}"
     const val ROOM_COMPILER = "androidx.room:room-compiler:${Versions.ROOM}"
+
+    // Calendar View
+    const val CALENDAR_VIEW =
+        "com.github.prolificinteractive:material-calendarview:${Versions.CALENDAR_VIEW}"
 }
