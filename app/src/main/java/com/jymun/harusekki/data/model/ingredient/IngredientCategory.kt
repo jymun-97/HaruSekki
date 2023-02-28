b4cc4163983551f470653f3e4e2ccbccb7282269
jymun-97 <mjy0750@gmail.com>
2023-02-28 21:55:25 +0900

    add: IngredientCategory Enum 정의 -> 필요 이미지, 문자열 리소스 추가

--- app/src/main/java/com/jymun/harusekki/data/model/ingredient/IngredientCategory.kt
+++ app/src/main/java/com/jymun/harusekki/data/model/ingredient/IngredientCategory.kt
@@ -1,11 +1,21 @@
 package com.jymun.harusekki.data.model.ingredient
 
-import com.jymun.harusekki.data.model.Model
-import com.jymun.harusekki.data.model.ModelType
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.jymun.harusekki.R
 
-data class IngredientCategory(
-    override val id: Long,
-    override val type: ModelType = ModelType.INGREDIENT_CATEGORY,
-    val name: String,
-    val imgResId: Int
-) : Model(id, type)
+enum class IngredientCategory(
+    val id: Int,
+    @StringRes val textResId: Int,
+    @DrawableRes val iconResId: Int
+) {
+    GRAIN(0, R.string.grain, R.drawable.ic_ingredient_grain),
+    MEAT(1, R.string.meat, R.drawable.ic_ingredient_meat),
+    VEGETABLE(2, R.string.vegetable, R.drawable.ic_ingredient_vegetable),
+    SEAFOOD(3, R.string.seafood, R.drawable.ic_ingredient_seafood),
+    SEASONING(4, R.string.seasoning, R.drawable.ic_ingredient_seasoning),
+    DAIRY_PRODUCT(5, R.string.dairy_product, R.drawable.ic_ingredient_dairy_products),
+    FRUIT(6, R.string.fruit, R.drawable.ic_ingredient_fruit),
+    NUTS(7, R.string.nuts, R.drawable.ic_ingredient_nuts),
+    OTHERS(8, R.string.others, R.drawable.ic_ingredient_others),
+}
