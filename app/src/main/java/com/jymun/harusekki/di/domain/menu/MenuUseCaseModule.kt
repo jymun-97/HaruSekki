0801cb7f5bd40780a162fb0a22636ac72787866a
jymun-97 <mjy0750@gmail.com>
2023-02-28 19:55:41 +0900

    feat: 특정 연도, 월에 식단 데이터를 갖는 CalendarDay 집합 로드 유즈케이스

--- app/src/main/java/com/jymun/harusekki/di/domain/menu/MenuUseCaseModule.kt
+++ app/src/main/java/com/jymun/harusekki/di/domain/menu/MenuUseCaseModule.kt
@@ -3,6 +3,7 @@ package com.jymun.harusekki.di.domain.menu
 import com.jymun.harusekki.data.repository.menu.MenuRepository
 import com.jymun.harusekki.domain.menu.DeleteMenuUseCase
 import com.jymun.harusekki.domain.menu.InsertMenuUseCase
+import com.jymun.harusekki.domain.menu.LoadDateUseCase
 import com.jymun.harusekki.domain.menu.LoadMenuUseCase
 import com.jymun.harusekki.util.dispatcher.DispatcherProvider
 import dagger.Module
@@ -34,4 +35,11 @@ class MenuUseCaseModule {
         menuRepository: MenuRepository
     ) =
         DeleteMenuUseCase(dispatcherProvider, menuRepository)
+
+    @Provides
+    fun provideLoadDateUseCase(
+        dispatcherProvider: DispatcherProvider,
+        menuRepository: MenuRepository
+    ) =
+        LoadDateUseCase(dispatcherProvider, menuRepository)
 }
