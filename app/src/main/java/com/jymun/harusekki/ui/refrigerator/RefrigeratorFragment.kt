2cd32c3f45e03f891a5d4082a2f3bfaa68e20d58
jymun-97 <mjy0750@gmail.com>
2023-02-28 21:56:25 +0900

    feat: Refrigerator Fragment base ui -> 재료 추가 버튼 클릭시 Ingredient Fragment로 이동

--- app/src/main/java/com/jymun/harusekki/ui/refrigerator/RefrigeratorFragment.kt
+++ app/src/main/java/com/jymun/harusekki/ui/refrigerator/RefrigeratorFragment.kt
@@ -2,26 +2,40 @@ package com.jymun.harusekki.ui.refrigerator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.jymun.harusekki.databinding.FragmentRefrigeratorBinding
 import com.jymun.harusekki.ui.base.BaseFragment
+import com.jymun.harusekki.ui.base.LoadState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RefrigeratorFragment : BaseFragment<RefrigeratorViewModel, FragmentRefrigeratorBinding>() {
 
     override val viewModel: RefrigeratorViewModel by viewModels()
 
     override fun getViewDataBinding() = FragmentRefrigeratorBinding.inflate(layoutInflater)
 
+    override fun setUpBinding() = binding.apply {
+        viewModel = this@RefrigeratorFragment.viewModel
+        lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun observeState() = viewModel.loadState.observe(viewLifecycleOwner) {
+        if (it is LoadState.Error)
+            Toast.makeText(requireActivity(), it.exception.message, Toast.LENGTH_SHORT).show()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tempTextView.text = "나의 냉장고"
+
+        initAddIngredientButton()
     }
 
-    override fun setUpBinding() = binding.apply {
-        // TODO("Not yet implemented")
-    }
-
-    override fun observeState() {
-        // TODO("Not yet implemented")
+    private fun initAddIngredientButton() = binding.addIngredientButton.setOnClickListener {
+        findNavController().navigate(
+            RefrigeratorFragmentDirections.actionFragmentRefrigeratorToFragmentIngredient()
+        )
     }
 }
--- app/src/main/java/com/jymun/harusekki/ui/refrigerator/RefrigeratorViewModel.kt
+++ app/src/main/java/com/jymun/harusekki/ui/refrigerator/RefrigeratorViewModel.kt
@@ -2,8 +2,10 @@ package com.jymun.harusekki.ui.refrigerator
 
 import com.jymun.harusekki.ui.base.BaseViewModel
 import com.jymun.harusekki.util.dispatcher.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class RefrigeratorViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider
 ) : BaseViewModel(dispatcherProvider) {
