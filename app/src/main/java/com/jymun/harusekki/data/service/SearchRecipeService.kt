4a9d119d50b583c85f3923d4c227cd8e4df207f3
jymun-97 <mjy0750@gmail.com>
2023-03-01 00:56:49 +0900

    feat: 식재료 검색 api 함수 추가

--- app/src/main/java/com/jymun/harusekki/data/service/SearchRecipeService.kt
+++ app/src/main/java/com/jymun/harusekki/data/service/SearchRecipeService.kt
@@ -1,5 +1,6 @@
 package com.jymun.harusekki.data.service
 
+import com.jymun.harusekki.data.entity.ingredient.IngredientEntity
 import com.jymun.harusekki.data.entity.recipe.RecipeEntity
 import retrofit2.Response
 import retrofit2.http.GET
@@ -33,4 +34,17 @@ interface SearchRecipeService {
     suspend fun searchByIngredient(
         @Query("ingredients") ingredientList: List<Long>
     ): Response<List<RecipeEntity>>
+
+    @GET("/ingredient/findall")
+    suspend fun loadAllIngredient(): Response<List<IngredientEntity>>
+
+    @GET("/ingredient/findbytitle")
+    suspend fun searchIngredientByTitle(
+        @Query("title") title: String
+    ): Response<List<IngredientEntity>>
+
+    @GET("/ingredient/findbycategory")
+    suspend fun searchIngredientByCategory(
+        @Query("ingredient_category") category: String
+    ): Response<List<IngredientEntity>>
 }
