a1042f18d37c8c00fb6d0f680eb378194566eab8
jymun-97 <mjy0750@gmail.com>
2023-03-09 00:11:11 +0900

    feat: MemoEntity PrimaryKey 설정 및 TypeConverter 생성

--- app/src/main/java/com/jymun/harusekki/data/db/memo/MemoDatabase.kt
+++ app/src/main/java/com/jymun/harusekki/data/db/memo/MemoDatabase.kt
@@ -2,12 +2,14 @@ package com.jymun.harusekki.data.db.memo
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.jymun.harusekki.data.entity.memo.MemoEntity
 
 @Database(
     entities = [MemoEntity::class],
     version = 1
 )
+@TypeConverters(MemoTypeConverters::class)
 abstract class MemoDatabase : RoomDatabase() {
 
     abstract fun dao(): MemoDao
--- app/src/main/java/com/jymun/harusekki/data/entity/memo/MemoEntity.kt
+++ app/src/main/java/com/jymun/harusekki/data/entity/memo/MemoEntity.kt
@@ -1,5 +1,6 @@
 package com.jymun.harusekki.data.entity.memo
 
+import androidx.room.PrimaryKey
 import com.jymun.harusekki.data.entity.Entity
 import com.jymun.harusekki.data.entity.ingredient.IngredientEntity
 
@@ -7,7 +8,8 @@ import com.jymun.harusekki.data.entity.ingredient.IngredientEntity
     tableName = "memo"
 )
 data class MemoEntity(
-    override val id: Long,
+    override val id: Long = 0,
+    @PrimaryKey(autoGenerate = false)
     val text: String,
     val ingredient: IngredientEntity? = null,
     val isChecked: Boolean = false
