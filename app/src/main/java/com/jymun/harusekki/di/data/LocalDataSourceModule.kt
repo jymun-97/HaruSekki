d75ce0a4f7c0af57f1d7e8079d9bfb1e858457cc
jymun-97 <mjy0750@gmail.com>
2023-03-08 23:47:55 +0900

    add: Memo LocalDataSource (get, insert, delete)

--- app/src/main/java/com/jymun/harusekki/di/data/LocalDataSourceModule.kt
+++ app/src/main/java/com/jymun/harusekki/di/data/LocalDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.jymun.harusekki.di.data
 
 import com.jymun.harusekki.data.source.ingredient.IngredientDateSource
 import com.jymun.harusekki.data.source.ingredient.IngredientLocalDataSource
+import com.jymun.harusekki.data.source.memo.MemoDataSource
+import com.jymun.harusekki.data.source.memo.MemoLocalDataSource
 import com.jymun.harusekki.data.source.menu.MenuDataSource
 import com.jymun.harusekki.data.source.menu.MenuLocalDataSource
 import com.jymun.harusekki.data.source.recipe.RecipeDataSource
@@ -41,4 +43,10 @@ abstract class LocalDataSourceModule {
     abstract fun bindsIngredientLocalDataSource(
         ingredientLocalDateSource: IngredientLocalDataSource
     ): IngredientDateSource.Local
+
+    @Binds
+    @Singleton
+    abstract fun bindsMemoLocalDataSource(
+        memoLocalDataSource: MemoLocalDataSource
+    ): MemoDataSource
 }
