93ef6b9dab8bc6174f9474db8036daa97d2e65b8
jymun-97 <mjy0750@gmail.com>
2023-03-13 14:26:31 +0900

    feat: 레시피 상세화면의 '좋아요' 버튼 클릭시 내가 좋아한 레시피에 추가 혹은 삭제. 해당 레시피의 좋아요 여부에 따라 버튼 색상 변경

--- app/src/main/java/com/jymun/harusekki/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/jymun/harusekki/ui/detail/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.jymun.harusekki.ui.detail
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jymun.harusekki.data.model.menu.Menu
@@ -7,6 +8,9 @@ import com.jymun.harusekki.data.model.menu.MenuCategory
 import com.jymun.harusekki.data.model.recipe.RecipeDetail
 import com.jymun.harusekki.domain.menu.InsertMenuUseCase
 import com.jymun.harusekki.domain.recipe.LoadRecipeDetailUseCase
+import com.jymun.harusekki.domain.recipe.favorite.DeleteFavoriteRecipeUseCase
+import com.jymun.harusekki.domain.recipe.favorite.InsertFavoriteRecipeUseCase
+import com.jymun.harusekki.domain.recipe.favorite.IsRecipeLikedUseCase
 import com.jymun.harusekki.ui.base.BaseViewModel
 import com.jymun.harusekki.util.dispatcher.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,14 +21,22 @@ class DetailViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider,
     private val loadRecipeDetailUseCase: LoadRecipeDetailUseCase,
     private val insertMenuUseCase: InsertMenuUseCase,
+    private val insertFavoriteRecipeUseCase: InsertFavoriteRecipeUseCase,
+    private val deleteFavoriteRecipeUseCase: DeleteFavoriteRecipeUseCase,
+    private val isRecipeLikedUseCase: IsRecipeLikedUseCase
 ) : BaseViewModel(dispatcherProvider) {
 
-    private val _recipeDetail = MutableLiveData<RecipeDetail>()
-    val recipeDetail: LiveData<RecipeDetail>
+    private val _recipeDetail = MutableLiveData<RecipeDetail?>()
+    val recipeDetail: LiveData<RecipeDetail?>
         get() = _recipeDetail
 
+    private val _liked = MutableLiveData(false)
+    val liked: LiveData<Boolean>
+        get() = _liked
+
     fun loadData(recipeId: Long) = onMainDispatcher {
-        _recipeDetail.postValue(loadRecipeDetailUseCase.invoke(recipeId))
+        _recipeDetail.postValue(loadRecipeDetailUseCase(recipeId))
+        _liked.postValue(isRecipeLikedUseCase(recipeId))
     }
 
     fun addMenu(year: Int, month: Int, dayOfMonth: Int, menuCategory: MenuCategory) =
@@ -41,4 +53,19 @@ class DetailViewModel @Inject constructor(
                 )
             )
         }
+
+    fun likeRecipe() = onMainDispatcher {
+        val recipe = recipeDetail.value ?: return@onMainDispatcher
+        val isLiked = liked.value ?: false
+
+        Log.d("# DetailViewModel", "$recipe")
+        Log.d("# DetailViewModel", "$isLiked")
+        
+        if (isLiked) {
+            deleteFavoriteRecipeUseCase(recipe)
+        } else {
+            insertFavoriteRecipeUseCase(recipe)
+        }
+        _liked.postValue(!isLiked)
+    }
 }
