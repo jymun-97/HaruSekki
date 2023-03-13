55bbd3d713bb636e1983289d3974fa9ed3f5cf87
jymun-97 <mjy0750@gmail.com>
2023-03-13 23:22:08 +0900

    feat: 장보기 메모 바로가기 화면 구성

--- app/src/main/java/com/jymun/harusekki/ui/home/shortcut/MemoShortcutFragment.kt
+++ app/src/main/java/com/jymun/harusekki/ui/home/shortcut/MemoShortcutFragment.kt
@@ -2,27 +2,46 @@ package com.jymun.harusekki.ui.home.shortcut
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.jymun.harusekki.databinding.ShortcutMemoBinding
 import com.jymun.harusekki.ui.base.BaseFragment
-import com.jymun.harusekki.ui.menu.MenuViewModel
+import com.jymun.harusekki.ui.home.HomeFragmentDirections
+import com.jymun.harusekki.ui.memo.MemoViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class MemoShortcutFragment : BaseFragment<MenuViewModel, ShortcutMemoBinding>() {
+@AndroidEntryPoint
+class MemoShortcutFragment : BaseFragment<MemoViewModel, ShortcutMemoBinding>() {
 
-    override val viewModel: MenuViewModel by activityViewModels()
+    override val viewModel: MemoViewModel by viewModels()
 
     override fun getViewDataBinding() = ShortcutMemoBinding.inflate(layoutInflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initMemoSeekbar()
+        initMoveToMemoFragmentButton()
+
+        viewModel.loadAllMemo()
     }
 
     override fun setUpBinding() = binding.apply {
-        // TODO("Not yet implemented")
+        viewModel = this@MemoShortcutFragment.viewModel
+        lifecycleOwner = viewLifecycleOwner
     }
 
-    override fun observeState() {
-        // TODO("Not yet implemented")
+    override fun observeState() {}
+
+    private fun initMemoSeekbar() = binding.memoSeekbar.apply {
+        isEnabled = false
+    }
+
+    private fun initMoveToMemoFragmentButton() = binding.moveToMenoFragmentButton.apply {
+        setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionFragmentHomeToFragmentMemo()
+            )
+        }
     }
 }
