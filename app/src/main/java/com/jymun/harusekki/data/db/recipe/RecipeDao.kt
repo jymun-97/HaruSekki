5dc17c2b3e0e69b3cb80cc395035a2330da3d6f6
jymun-97 <mjy0750@gmail.com>
2023-03-13 14:23:53 +0900

    feat: '내가 좋아한 레시피' db 테이블 및 dao 정의

--- app/src/main/java/com/jymun/harusekki/data/db/recipe/RecipeDao.kt
+++ app/src/main/java/com/jymun/harusekki/data/db/recipe/RecipeDao.kt
@@ -1,10 +1,8 @@
 package com.jymun.harusekki.data.db.recipe
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.jymun.harusekki.data.entity.recipe.LatestReadRecipe
+import com.jymun.harusekki.data.entity.recipe.RecipeEntity
 
 @Dao
 interface RecipeDao {
@@ -21,4 +19,22 @@ interface RecipeDao {
                 "(SELECT id FROM latest_read_recipe ORDER BY timestamp DESC LIMIT 10)"
     )
     suspend fun deleteOldestReadRecipe()
+
+    @Query("SELECT * FROM favorite_recipe ORDER BY id")
+    suspend fun loadLatestFavoriteRecipe(): List<RecipeEntity>
+
+    @Query("SELECT * FROM favorite_recipe ORDER BY hits DESC")
+    suspend fun loadMostHitsFavoriteRecipe(): List<RecipeEntity>
+
+    @Query("SELECT * FROM favorite_recipe ORDER BY likes DESC")
+    suspend fun loadMostLikesFavoriteRecipe(): List<RecipeEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(recipe: RecipeEntity)
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(recipe: RecipeEntity)
+
+    @Query("SELECT EXISTS(SELECT * FROM favorite_recipe WHERE id = :id)")
+    suspend fun isRecipeLiked(id: Long): Boolean
 }
--- app/src/main/java/com/jymun/harusekki/data/db/recipe/RecipeDatabase.kt
+++ app/src/main/java/com/jymun/harusekki/data/db/recipe/RecipeDatabase.kt
@@ -4,9 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.jymun.harusekki.data.entity.recipe.LatestReadRecipe
+import com.jymun.harusekki.data.entity.recipe.RecipeEntity
 
 @Database(
-    entities = [LatestReadRecipe::class],
+    entities = [LatestReadRecipe::class, RecipeEntity::class],
     version = 1
 )
 @TypeConverters(RecipeTypeConverters::class)
